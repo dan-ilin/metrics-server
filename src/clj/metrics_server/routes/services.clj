fc2ebb2b90c28aa564b7ad352ff64f3f679ad833
Dan Ilin <dan.ilin@me.com>
2016-05-05 12:45:48 -0400

    Add POST and GET routes and tests.

--- src/clj/metrics_server/routes/services.clj
+++ src/clj/metrics_server/routes/services.clj
@@ -1,43 +1,37 @@
 (ns metrics-server.routes.services
   (:require [ring.util.http-response :refer :all]
             [compojure.api.sweet :refer :all]
-            [schema.core :as s]))
+            [schema.core :as s]
+            [metrics-server.db.core :as db]))
+
+(s/defschema Metric
+  {(s/optional-key :id) s/Int
+   :name                s/Str
+   :value               s/Num
+   :timestamp           s/Inst})
+
+(defn get-page-params [page-num]
+  (let [page-size 50]
+    {:limit  page-size
+     :offset (* page-size page-num)}))
 
 (defapi service-routes
-  {:swagger {:ui "/swagger-ui"
-             :spec "/swagger.json"
-             :data {:info {:version "1.0.0"
-                           :title "Sample API"
-                           :description "Sample Services"}}}}
-  (context "/api" []
-    :tags ["thingie"]
+        {:swagger {:ui   "/swagger-ui"
+                   :spec "/swagger.json"
+                   :data {:info {:version     "1.0.0"
+                                 :title       "Metrics API"
+                                 :description "Services for inserting and querying metrics."}}}}
+        (context "/api" []
+                 :tags ["metrics"]
 
-    (GET "/plus" []
-      :return       Long
-      :query-params [x :- Long, {y :- Long 1}]
-      :summary      "x+y with query-parameters. y defaults to 1."
-      (ok (+ x y)))
+                 (POST "/metric" []
+                       :return Integer
+                       :body [metric Metric]
+                       :summary "Insert single metric into database."
+                       (ok (db/insert-metric! metric)))
 
-    (POST "/minus" []
-      :return      Long
-      :body-params [x :- Long, y :- Long]
-      :summary     "x-y with body-parameters."
-      (ok (- x y)))
-
-    (GET "/times/:x/:y" []
-      :return      Long
-      :path-params [x :- Long, y :- Long]
-      :summary     "x*y with path-parameters"
-      (ok (* x y)))
-
-    (POST "/divide" []
-      :return      Double
-      :form-params [x :- Long, y :- Long]
-      :summary     "x/y with form-parameters"
-      (ok (/ x y)))
-
-    (GET "/power" []
-      :return      Long
-      :header-params [x :- Long, y :- Long]
-      :summary     "x^y with header-parameters"
-      (ok (long (Math/pow x y))))))
+                 (GET "/metric" []
+                      :return [Metric]
+                      :query-params [page :- Long]
+                      :summary "Get page of metrics."
+                      (ok (db/get-metrics (get-page-params page))))))
--- test/clj/metrics_server/test/handler.clj
+++ test/clj/metrics_server/test/handler.clj
@@ -1,13 +1,35 @@
 (ns metrics-server.test.handler
   (:require [clojure.test :refer :all]
             [ring.mock.request :refer :all]
-            [metrics-server.handler :refer :all]))
+            [metrics-server.handler :refer :all]
+            [cheshire.core :as json])
+  (:import (java.util Date)))
+
+(def test-metric
+  {:name      "Test"
+   :value     10.0
+   :timestamp (new Date)})
 
 (deftest test-app
-  (testing "main route"
-    (let [response ((app) (request :get "/"))]
+  (testing "swagger redirect"
+    (let [response ((app) (request :get "/swagger-ui"))]
+      (is (= 302 (:status response)))))
+
+  (testing "swagger ui"
+    (let [response ((app) (request :get "/swagger-ui/index.html"))]
       (is (= 200 (:status response)))))
 
   (testing "not-found route"
     (let [response ((app) (request :get "/invalid"))]
-      (is (= 404 (:status response))))))
+      (is (= 404 (:status response)))))
+
+  (testing "insert metric route"
+    (let [response ((app) (content-type
+                            (request :post "/api/metric"
+                                     (json/generate-string test-metric))
+                            "application/json"))]
+      (is (= 200 (:status response)))))
+
+  (testing "get metric route"
+    (let [response ((app) (request :get "/api/metric" {:page 0}))]
+      (is (= 200 (:status response))))))
